3f3e55b80d20ec15f4c4a9d2400d79988431ae73
Polkhoovskaya <37327952+Polkhoovskaya@users.noreply.github.com>
2026-06-27 20:55:59 +0000

    Add: coutses to the UI

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from the Shop app.")
+    courses = models.Course.objects.all()
+    return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
