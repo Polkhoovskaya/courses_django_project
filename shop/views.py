c5dfc0fe5e5d789d1508960c36d330f598f17579
Polkhoovskaya <37327952+Polkhoovskaya@users.noreply.github.com>
2026-06-28 11:05:31 +0000

    Add: single course template

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from . import models
 
 # Create your views here.
 def index(request):
     courses = models.Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'shop/courses.html', {'courses': courses})
+
+def single_course(request, course_id):
+    try:
+        course = models.Course.objects.get(pk=course_id)
+        return render(request, 'shop/single_course.html', {'course': course})
+    except models.Course.DoesNotExist:
+        raise Http404("Course not found")
